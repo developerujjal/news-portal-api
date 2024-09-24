bc8515171917c18f1da7183e43e7c90e988e4278
Developer Ujjal <ujjalchandra213@gmail.com>
2024-09-24 08:50:04 +0600

    Completed API New Portal

--- js/news-portal.js
+++ js/news-portal.js
@@ -1,3 +1,6 @@
+/* Categories Display Start*/
+
+
 const categoriesDataLoad = async () => {
     const response = await fetch('https://openapi.programming-hero.com/api/news/categories');
     const data = await response.json();
@@ -5,23 +8,32 @@ const categoriesDataLoad = async () => {
 
     // Categories Appended as a Child
     const ulCategoryElement = document.getElementById('category-main-list');
-    dataShow.forEach((category) => {
+    // ulCategoryElement.classList.add('category-iteam');
+    dataShow.forEach((category, event) => {
 
         const liCategory = document.createElement('li');
         liCategory.innerHTML = `
-                <a onclick="handleCategoryNews('${category.category_id}')" href="#">${category.category_name}</a>
+                <a onclick="handleCategoryNews('${category.category_id}')" class="category-iteam" href="#">${category.category_name}</a>
         `;
 
         ulCategoryElement.appendChild(liCategory);
 
     })
+
+
 }
 
 
+/* Categories Display End*/
 
-// Access Categories NEWS
+
+
+
+/* Display Categories NEWS Start */
+
 const handleCategoryNews = async (categoryId) => {
     loadSpinner(true);
+
     const response = await fetch(`https://openapi.programming-hero.com/api/news/category/${categoryId}`);
     const data = await response.json();
     const allDataShow = data.data;
@@ -34,7 +46,44 @@ const handleCategoryNews = async (categoryId) => {
     const newsWrapperContainer = document.getElementById('main-news-box');
     newsWrapperContainer.innerHTML = "";
 
+
+    // loadingSpinner Close
+    if (allDataShow.length > 0) {
+        loadSpinner(false);
+    }
+
+
+
+    /* Selected Color  Start*/
+
+    const categories = document.querySelectorAll('.category-iteam');
+    for (let category of categories) {
+        category.addEventListener('click', function (event) {
+
+            // categories.forEach((cate => {
+            //     cate.style.color = "";
+            //     cate.style.backgroundColor = "";
+            // }));
+
+            for(let cate of categories){
+                cate.style.color = "";
+                cate.style.backgroundColor = "";
+            }
+
+
+            const targetEvent = event.target;
+            targetEvent.style.color = "#5D5FEF";
+            targetEvent.style.backgroundColor = "#EEEFFF";
+        })
+    }
+
+    /* Selected Color End*/
+    
+
+
     allDataShow.forEach((news) => {
+        // loadSpinner(false); we also used it here for stoping the spinner
+
         const newDiv = document.createElement('div');
         newDiv.classList.add('news-singel-box-wrapper');
         newDiv.innerHTML = `
@@ -45,7 +94,7 @@ const handleCategoryNews = async (categoryId) => {
                         <div class="news-des-container">
                             <div class="only-news-des">
                                 <h2>${news.title}</h2>
-                                <p>${news.details}</p>
+                                <p>${news.details.slice(0, 400)}...</p>
                             </div>
                             <div class="author-container">
                                 <div class="author">
@@ -60,7 +109,7 @@ const handleCategoryNews = async (categoryId) => {
                                 <div class="news-views-container">
                                     <div class="news-views">
                                         <i class="material-icons">visibility</i>
-                                        <span>${news?.total_view ? news?.total_view: "No Views"}</span>
+                                        <span>${news?.total_view ? news?.total_view : "No Views"}</span>
                                     </div>
                                 </div>
                                 <div class="rating-star">
@@ -78,22 +127,26 @@ const handleCategoryNews = async (categoryId) => {
        
        `;
 
-       newsWrapperContainer.appendChild(newDiv);
+        newsWrapperContainer.appendChild(newDiv);
 
-       loadSpinner(false);
+
     })
 
 }
 
+/* Display Categories NEWS Start */
 
-// Loading Spinner
+
+
+
+/* Loading Spinner */
 const loadSpinner = (spinner) => {
-    const  loaderSpinnerElement = document.getElementById('loaderSpinner');
-    if(spinner){
-       loaderSpinnerElement.classList.remove('hidden');
+    const loaderSpinnerElement = document.getElementById('loaderSpinner');
+    if (spinner) {
+        loaderSpinnerElement.classList.remove('hidden');
     }
 
-    else{
+    else {
         loaderSpinnerElement.classList.add('hidden')
     }
 }
@@ -101,7 +154,23 @@ const loadSpinner = (spinner) => {
 
 
 
-// Modal
+/* Handle Search Bar */
+
+const handleSearchBar = () => {
+    const inputFeild = document.getElementById('input-feild');
+    const inputFeildValue = inputFeild.value;
+    console.log(typeof inputFeildValue)
+    if (inputFeildValue) {
+        handleCategoryNews(inputFeildValue);
+    }
+    else {
+        alert("Please search by category ID!");
+    }
+}
+
+
+
+/* Modal Start */
 
 const handleModal = async (newsID) => {
     const response = await fetch(`https://openapi.programming-hero.com/api/news/${newsID}`);
@@ -131,6 +200,11 @@ const handleCloseBtn = () => {
     const myModalElement = document.getElementById('myModal');
     myModalElement.classList.add('hidden');
 }
+
+
+/* Modal End */
+
+
 
 categoriesDataLoad();
 handleCategoryNews('08');
